47078e8ceec402f0a9af73d98525d1c7d12c3b56
leebaba20 <princeleeoye@gmail.com>
2025-04-30 15:42:10 +0100

    Removed Success.css file and cleaned up imports

--- src/components/pages/Success.jsx
+++ src/components/pages/Success.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './success.css';
 
 const Success = () => {
   const [order, setOrder] = useState(null);
