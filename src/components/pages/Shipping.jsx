5006676325015a86523c76c756487f31f4b4f532
leebaba20 <princeleeoye@gmail.com>
2025-05-02 01:23:31 +0100

    Fix broken import path for useCart

--- src/components/pages/Shipping.jsx
+++ src/components/pages/Shipping.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useCart } from '../../hooks/useCart';
-import UseUser from '../../hooks/useUser';
+import { useCart } from '../../../hooks/useCart';
+import UseUser from '../../../hooks/useUser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Button, Spinner } from 'react-bootstrap';
