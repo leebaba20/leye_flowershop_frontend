317164f7ac4a18e4631af523331ef7037a188608
leebaba20 <princeleeoye@gmail.com>
2025-06-12 15:34:32 +0100

    fix Netlify build config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import App component
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* App component will now handle the BrowserRouter */}
+    <App />
   </React.StrictMode>
 );
