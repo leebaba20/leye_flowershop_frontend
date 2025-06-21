485335b20774269cf51865e14c18d7843d47b190
leebaba20 <princeleeoye@gmail.com>
2025-06-21 22:36:15 +0100

    � Update: Added search results, merged products, and new CSS styling

--- src/components/pages/SearchResults.jsx
+++ src/components/pages/SearchResults.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Card from '../commonfiles/Card';
-import './SearchResults.css'; 
+import './searchResults.css'; 
 
 const SearchResults = () => {
   const location = useLocation();
